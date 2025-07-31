33369b3d22c41d5692c6572942966e7f0911cb74
aelin <rezende@espressolabs.com.br>
2025-07-31 20:07:45 -0300

    Base do código: Cria repositório base e refatora a conexão com o banco de dados

---
+++ src/backend/modules/user/user_repository.py
@@ -0,0 +1,8 @@
+from backend.modules.base.base_repository import BaseRepository
+
+
+class UserRepository(BaseRepository):
+    """
+    Repositório para operações relacionadas a usuários.
+    """
+    pass
