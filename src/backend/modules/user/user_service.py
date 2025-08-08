6fb037edcbbca8a99ac5f79788467eeb20e8ecd2
aelin <rezende@espressolabs.com.br>
2025-08-08 20:35:22 -0300

    Remove prints de depuração do `UserService`

--- src/backend/modules/user/user_service.py
+++ src/backend/modules/user/user_service.py
@@ -13,12 +13,10 @@ from backend.modules.user.user_repository import UserRepository
 class UserService(BaseService[User]):
     def __init__(self, repository: UserRepository = Depends(), mailgun: MailGun = Depends()):
         super().__init__(repository, User)
-        print("Inicializando UserService com Mailgun")
         self.mailgun = mailgun
 
     # TODO: Remover no futuro
     async def send_test_email(self, content: str) -> bool:
-        print(f"Enviando email de teste {self.mailgun}")
         return await self.mailgun.send_email(
             settings.MAILGUN_TEST_EMAIL,
             "Teste de Email",
