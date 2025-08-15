33bc02d570d0826fd34fa1658816d5258b554466
aelin <rezende@espressolabs.com.br>
2025-08-14 21:27:00 -0300

    `UserRouter`: Injetar dependência `UserService`

--- src/backend/modules/user/user_router.py
+++ src/backend/modules/user/user_router.py
@@ -1,15 +1,22 @@
 
+from fastapi.params import Depends
+from fastapi_utils.cbv import cbv
 from fastapi_utils.inferring_router import InferringRouter
+from wireup import service as inject
 
 from backend.models.user import User
 from backend.modules.base.base_router import get_base_router
+from backend.modules.user.user_service import UserService
 
 user_router = InferringRouter(prefix="/users", tags=["Users"])
 BaseRouter = get_base_router(user_router)
 
 
+@inject(lifetime="scoped")
+@cbv(user_router)
 class UserRouter(BaseRouter[User]):
     """Router para o modelo de usuário"""
 
-    def __init__(self):
+    def __init__(self, service: UserService = Depends()):
         """Inicializa o router com serviços"""
+        super().__init__(service)
