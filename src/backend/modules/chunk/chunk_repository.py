afd750e8df85f9912afe2aa144d4dbbe289e654c
aelin <rezende@espressolabs.com.br>
2025-09-21 19:59:17 -0300

    `ChunkRepository`: Adiciona método `get_similar` para buscar chunks similares com base em um embedding

--- src/backend/modules/chunk/chunk_repository.py
+++ src/backend/modules/chunk/chunk_repository.py
@@ -1,9 +1,14 @@
+from typing import Callable, List, Optional
+
 from fastapi.params import Depends
+from sqlmodel.sql.expression import SelectOfScalar
 from wireup import service
 
 from backend.core.database import DatabaseConnection
 from backend.models.chunk import Chunk
 from backend.modules.base.base_repository import BaseRepository
+
+Builder = Callable[[SelectOfScalar[Chunk]], SelectOfScalar[Chunk]]
 
 
 @service(lifetime="scoped")
@@ -12,3 +17,24 @@ class ChunkRepository(BaseRepository[Chunk]):
 
     def __init__(self, connection: DatabaseConnection = Depends()):
         super().__init__(Chunk, connection)
+
+    async def get_similar(
+        self, embedding: List[float],
+        builder: Optional[Builder] = None,
+        limit: int = 10
+    ) -> List[Chunk]:
+        """Busca chunks similares baseado em um embedding fornecido."""
+        query = self.query.order_by(
+            Chunk.embedding.cosine_distance(  # pylint: disable=no-member
+                embedding
+            )
+        ).limit(limit)
+
+        if builder:
+            query = builder(query)
+
+        result = await self.run(
+            query
+        )
+
+        return result.scalars().all()
