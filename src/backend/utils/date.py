bed3758c39e197065a1a68e72abb42087c3f785a
aelin <rezende@espressolabs.com.br>
2025-10-28 21:00:20 -0300

    Utilitários (date): Adiciona função `is_past`

--- src/backend/utils/date.py
+++ src/backend/utils/date.py
@@ -5,12 +5,38 @@ from datetime import datetime, timedelta, timezone
 
 def add_days(days: int) -> datetime:
     """
-        Adiciona dias à data atual.
+    Adiciona dias à data atual.
 
-        Args:
-            days: Número de dias a adicionar
+    Args:
+        days: Número de dias a adicionar
 
-        Returns:
-            datetime: Data resultante
-        """
+    Returns:
+        datetime: Data resultante
+    """
     return datetime.now(timezone.utc) + timedelta(days=days)
+
+
+def is_past(date_to_check: datetime) -> bool:
+    """
+    Verifica se uma data está no passado.
+
+    Args:
+        date_to_check: Data a ser verificada
+
+    Returns:
+        bool: True se a data estiver no passado
+    """
+    return _remove_time_zone(date_to_check) < datetime.now()
+
+
+def _remove_time_zone(date: datetime) -> datetime:
+    """
+    Remove a informação de fuso horário de um objeto datetime.
+
+    Args:
+        dt: Objeto datetime com fuso horário
+
+    Returns:
+        datetime: Objeto datetime sem fuso horário
+    """
+    return date.replace(tzinfo=None)
